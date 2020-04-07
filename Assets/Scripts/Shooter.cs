51c18d4e68baf04815ce98dc692024cb0ec9e044
BillNyeTheGamerGuy <howard327@morrisville.edu>
2020-04-07 16:41:06 -0400

    Instantiate from Gun Position

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    //Instantiates the projectile
+    [SerializeField] GameObject projectile, gun;
+
+   //Fires a projectile
+   public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, transform.rotation);
+    }
+}
