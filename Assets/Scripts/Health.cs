7f3a5e237cf93cc971aabcc1958e7a943f0059d4
BillNyeTheGamerGuy <howard327@morrisville.edu>
2020-04-07 18:31:09 -0400

    Implementing Health & Damage

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    //Amount of Health
+    [SerializeField] float fltHealth = 100f;
+
+    //Dealing Damage and how MUCH damage to deal.
+    public void DealDamage(float fltDamage)
+    {
+        //Decrease health on hit/damage dealt
+        fltHealth -= fltDamage;
+        if (fltHealth <= 0)
+        {
+            Destroy(gameObject);
+        }//Delete the game object.
+    }
+
+
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,10 +6,22 @@ public class Projectile : MonoBehaviour
 {
     //Defines our fltSpeed variable's default speed
     [SerializeField] float fltSpeed = 1f;
+    [SerializeField] float fltDamage = 50;
 
     // Moves our Projectile
     void Update()
     {
         transform.Translate(Vector2.right * fltSpeed * Time.deltaTime);
     }//moves our projectile to the right multiplied by the fltSpeed * Time.deltaTime 
+
+    //Trigger collider for other things/things the projectile collides into.
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var Health = otherCollider.GetComponent<Health>();
+        //Reducing Health
+        Health.DealDamage(fltDamage);
+    }
+
+
+
 }
