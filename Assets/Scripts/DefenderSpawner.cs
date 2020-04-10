5d2345f5df6e488290c323899a77c05165424c96
BillNyeTheGamerGuy <howard327@morrisville.edu>
2020-04-09 22:29:02 -0400

    Snap Defenders to Grid

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -16,17 +16,31 @@ public class DefenderSpawner : MonoBehaviour
     private Vector2 GetSquareClicked()
     {   //defines the position of a clicking mouse.
         Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
         //Translate clickPos to ScreenToWorldPoint
-        return worldPos;
-        //Returns worldPos to the Vector2 of 'GetSquareClicked()'
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        //Convers worldPos to gridPos
+        Vector2 gridPos = SnapToGrid(worldPos);
+        return gridPos;
+        //Returns gridPos to the Vector2 of 'GetSquareClicked()'
+    }
+
+    //Helps convert decimals to integers.
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        //Rounds X and Y position of rawWorldPos to integers
+        float fltNewX = Mathf.RoundToInt(rawWorldPos.x);
+        float fltNewY = Mathf.RoundToInt(rawWorldPos.y);
+        //Returns fltNewX and fltNewY.
+        return new Vector2(fltNewX, fltNewY);
+
     }
 
 
     //On click will spawn Cactus
-    private void SpawnDefender(Vector2 worldPos)
+    private void SpawnDefender(Vector2 roundedPos)
     {
-        GameObject newDefender = Instantiate(defender, worldPos, Quaternion.identity) as GameObject;
+        GameObject newDefender = Instantiate(defender, roundedPos, Quaternion.identity) as GameObject;
+        Debug.Log(roundedPos);
     }
 
 }
