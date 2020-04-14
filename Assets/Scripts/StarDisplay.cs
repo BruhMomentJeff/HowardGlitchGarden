ae512c8ef00806ff47e64e44367de2a4c46b98f3
BillNyeTheGamerGuy <howard327@morrisville.edu>
2020-04-14 18:14:11 -0400

    Start Our Resource System

---
+++ Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarDisplay : MonoBehaviour
+{
+    //Displays the amount of stars the player will have.
+    [SerializeField] int intStars = 100;
+    Text strStarText;
+
+    void Start()
+    {   //Defines strStarText as a .text
+        strStarText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {   //Converts anything in intStars to a string type.
+        strStarText.text = intStars.ToString();
+    }
+    
+    public void AddStars(int intAmount)
+    {
+        intStars += intAmount;
+        UpdateDisplay();
+    }
+
+    public void SpendStars(int intAmount)
+    {
+        if (intStars >= intAmount) ;
+        {
+            intStars -= intAmount;
+            UpdateDisplay();
+        }
+    }
+
+}
