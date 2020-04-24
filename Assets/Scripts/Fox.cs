993f50eb3a81eb91e4069f3597258f4109553b5b
BillNyeTheGamerGuy <howard327@morrisville.edu>
2020-04-23 20:22:23 -0400

    Fantastic Mr. Fox

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+   private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject otherObject = otherCollider.gameObject;
+        //Determines if what they encounter is an 'Gravestone' type, they're just jump over it with 'JumpTrigger'
+        if(otherObject.GetComponent<Gravestone>())
+        {
+            GetComponent<Animator>().SetTrigger("JumpTrigger");
+        }
+
+        //Determines else if what they 'hit' is an 'Defender' type, they'll attack, otherwise they won't.
+        else if(otherObject.GetComponent<Defender>())
+        {
+            GetComponent<Attacker>().Attack(otherObject);
+        }
+    }
+}
