3fd7ac34ba22e19031a39f55e42ecf24ca936ef4
BillNyeTheGamerGuy <howard327@morrisville.edu>
2020-04-30 19:41:50 -0400

    A Nicer Win Experience

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
+    [SerializeField] float fltWaitToLoad = 4f;
+    [SerializeField] GameObject winLabel;
     //Helps determine how many attackers are remaining
     int intNumberOfAttackers = 0;
     bool boolLevelTimerFinished = false;
+
+    private void Start()
+    {
+        winLabel.SetActive(false);
+    }
 
     //Checks for when an attacker is spawned that it counts up any new attackers
     //into intNumberOfAttackers
@@ -20,9 +27,20 @@ public class LevelController : MonoBehaviour
         intNumberOfAttackers--;
         if(intNumberOfAttackers <= 0 && boolLevelTimerFinished)
         {
-            Debug.Log("Level Over!");
+            StartCoroutine(HandleWinCondition());
         }
     }
+    //Activates WinLabel by setting it to true.
+    //Plays the audio source after waiting 'for seconds'
+    //And then LoadsNextScene
+    IEnumerator HandleWinCondition()
+    {
+        winLabel.SetActive(true);
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(fltWaitToLoad);
+        FindObjectOfType<LevelLoader>().LoadNextScene();
+    }
+
     //Checks for when the Level Timer has Finished and stops spawning attacks.
     public void boollevelTimerFinished()
     {
