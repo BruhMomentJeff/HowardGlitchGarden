526a5d521e6c0bdeca2350c7250751160cd462f0
BillNyeTheGamerGuy <howard327@morrisville.edu>
2020-05-05 23:17:53 -0400

    Set & Save Music Volume

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    AudioSource audioSource;
+
+
+    //Prevents 'this' from being destroyed upon load and keeps playing throughout
+    //the entire game.
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = PlayerPrefsController.GetMasterVolume();
+    }
+    //sets volume
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+}
---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] float defaultVolume = 0.8f;
+
+    //Is utilizing the slider for volume and it's value.
+    void Start()
+    {
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+    }
+
+    //Allows easy access of Music Player
+    void Update()
+    {
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+        if (musicPlayer) //For if the musicPlayer is already in use.
+        {
+            musicPlayer.SetVolume(volumeSlider.value);
+        }
+        else //Will occur if you don't start from the splash screen.
+        {
+            Debug.LogWarning("No music player found, did you start from the splash screen?");
+        }
+    }
+    //Saves your settings when you Exit options screen.
+    public void SaveAndExit()
+    {
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+        FindObjectOfType<LevelLoader>().LoadMainMenu();
+    }
+    //Upon selecting Defaults, it will revert the slider back to the defaultVolume.
+    public void SetDefaults()
+    {
+        volumeSlider.value = defaultVolume;
+    }
+}
