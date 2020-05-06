e6eba876ec96974796f6502dfc399cc58c3aa928
BillNyeTheGamerGuy <howard327@morrisville.edu>
2020-05-05 21:22:43 -0400

    PlayerPrefs To Save Settings

---
+++ Assets/Scripts/PlayerPrefsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsController : MonoBehaviour
+{//Makes sure that the options these are assigned to are constant
+    const string MASTER_VOLUME_KEY = "master volume";
+    const string DIFFICULTY_KEY = "difficulty";
+
+    const float MIN_VOLUME = 0f;
+    const float MAX_VOLUME = 1f;
+
+    //Creates our MasterVolume slider with fltvolume with a check and balance of
+    //MIN_VOLUME and MAX_VOLUME as it's range
+    public static void SetMasterVolume(float volume)
+    {
+        if (volume >= MIN_VOLUME && volume <= MAX_VOLUME)
+        {
+            Debug.Log("Master volume set to " + volume);
+            PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+        }
+        else //Alerts if master volume is out of range with an error in console.
+        {
+            Debug.LogError("Master Volume is out of range.");
+        }
+    }
+    //Returns/Gets MasterVolume
+    public static float GetMasterVolume()
+    {
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+    }
+
+}
