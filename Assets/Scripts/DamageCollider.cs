1e564d5096ea72219e6abf4039fee1322fc06479
BillNyeTheGamerGuy <howard327@morrisville.edu>
2020-05-07 18:21:32 -0400

    Quick Bug Fix – Win Condition

--- Assets/Scripts/DamageCollider.cs
+++ Assets/Scripts/DamageCollider.cs
@@ -6,8 +6,9 @@ public class DamageCollider : MonoBehaviour
 {
     //Upon contact with Trigger, activates TakeLife, 
     //reducing the amount of Life(points) left by 1.
-    private void OnTriggerEnter2D()
+    private void OnTriggerEnter2D(Collider2D otherCollider)
     {
         FindObjectOfType<LivesDisplay>().TakeLife();
+        Destroy(otherCollider.gameObject);
     }
 }
